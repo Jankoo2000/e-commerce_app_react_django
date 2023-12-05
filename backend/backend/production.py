6127802718bc72a1a6f766ca8d966ea5965ab974
Jankoo2000 <93423845+Jankoo2000@users.noreply.github.com>
2023-12-05 19:27:28 +0100

    debug = True

--- backend/backend/production.py
+++ backend/backend/production.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
-DEBUG = False
+DEBUG = True
 
 
 INSTALLED_APPS = [

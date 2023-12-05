d1fce5ec0e4b91933dc5d1d985e9a6c28c85f46d
Jankoo2000 <93423845+Jankoo2000@users.noreply.github.com>
2023-12-05 20:06:06 +0100

    moved all to settings and debug=True

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -13,7 +13,7 @@ from django.core.wsgi import get_wsgi_application
 
 settings_module = 'backend.production' if 'WEBSITE_HOSTNAME' in os.environ else 'backend.settings'
 
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()

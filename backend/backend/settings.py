0a8569493595c3f414bc8f6c8361d11f80388bef
Jankoo2000 <93423845+Jankoo2000@users.noreply.github.com>
2023-12-05 21:14:44 +0100

    Merge remote-tracking branch 'origin/deployment_cbi_analytics_restored' into deployment_cbi_analytics_restored
    # Conflicts:
    #	backend/backend/settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
+#
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-7^0er-(fs)dm$uhf6*e-l=+nl1#((t*1(e-@frnl0k!clnixz*'
 

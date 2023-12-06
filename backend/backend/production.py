55ba834f542856259ed13820ec52100e782fe8a8
Jankoo2000 <93423845+Jankoo2000@users.noreply.github.com>
2023-12-06 09:23:47 +0100

    CSRF_TRUSTED_ORIGINS in list and static url         const {data} = await axios.get(`https://e-commarce.azurewebsites.net/api/products${keyword}`)

--- backend/backend/production.py
+++ backend/backend/production.py
@@ -20,14 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
-DEBUG = True
+# DEBUG = True
 
-# CORS_ALLOWED_ORIGINS = [
-#     "https://agreeable-bush-0fae7cb0f.4.azurestaticapps.net",
-#     'https://agreeable-bush-0fae7cb0f.4.azurestaticapps.net/'
-# ]
+CORS_ALLOWED_ORIGINS = [
+    "https://agreeable-bush-0fae7cb0f.4.azurestaticapps.net",
+    'https://agreeable-bush-0fae7cb0f.4.azurestaticapps.net/'
+]
 
-CORS_ALLOWED_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = True
 
 
 INSTALLED_APPS = [
